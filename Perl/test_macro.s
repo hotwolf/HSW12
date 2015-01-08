27b5e585f75c7ba6afc2a5ab7362bb7bcbb1128f
hotwolf <github@hotwolf.de>
2015-01-08 19:34:35 +0100

    added precompiler directives #ifmac and #ifnmac

--- Perl/test_macro.s
+++ Perl/test_macro.s
@@ -1,14 +1,30 @@
 #MACRO MAC0 2       
-       MAC1  \1
-LOOP   CPD   0,\2
-       BEQ   LOOP
+		MAC1  \1
+LOOP   		CPD   0,\2
+		BEQ   LOOP
 #EMAC
 
 #MACRO MAC1 1       
-LOOP   CPD   0,\1       
-       BNE   LOOP
+LOOP   		CPD   0,\1       
+		BNE   LOOP
 #EMAC       
 
-       CPU   S12	
-       ORG   $0000
-       MAC0  X, Y
+
+		CPU   S12	
+		ORG   $0000
+		MAC0  X, Y
+
+#ifmac MAC0	
+MAC0_DEFFINED	EQU	1	
+#else
+MAC0_UNDEFFINED	EQU	1	
+#endif
+
+#ifnmac MAC1	
+MAC1_UNDEFFINED	EQU	1	
+#else
+MAC1_DEFFINED	EQU	1	
+#endif
+
+		NOP
+		NOP
